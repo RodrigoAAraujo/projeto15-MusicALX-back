5bf116e1257cb30696468850eb0494301bb31a27
RodrigoAAraujo <rodrigo.teth@gmail.com>
2022-11-25 17:14:26 -0300

    feat: Product post finished

--- src/models/productSchemas.js
+++ src/models/productSchemas.js
@@ -1,11 +1,11 @@
 import joi from "joi";
 
 export const ProductSchema = joi.object({
-    product: joi.string(3).max(30).min(3).required(),
-    seller: joi.string(3).max(20).required(),
+    product: joi.string().max(30).min(3).required(),
+    seller: joi.string().max(20).required(),
     type: joi.string().required(),
     qtd: joi.number().min(1).required(),
     value: joi.string().min(3).required(),
-    image: joi.string().required(),
+    image: joi.required(),
     copy: joi.string().min(10).max(400).required()
 })
